fca7b8a1bc37355f541bb2d42b03d91a9c7b9794
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-05-27 17:20:39 +0900

    commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// オープニング
+$(function() {
+	setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},400); //0.4秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},2000); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
