9d560a5ade9a3a23d56b013b982c2b9a594a7eaf
koki sugano <c9xj.c9ajmf2@gmail.com>
2025-06-07 11:45:42 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ $(function() {
 		$('.start').fadeOut(500);
 	},2000); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
 });
+
+
+jQuery(function ($) {
+	var fadeIn = $('.fade-in');
+	$(window).scroll(function () {
+		$(fadeIn).each(function () {
+			var offset = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > offset - windowHeight + 150) {
+				$(this).addClass("scroll-in");
+			}
+		});
+	});
+});
